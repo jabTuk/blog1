21b5311acb423d3870ca03fa1e65d6aa5eb57b89
t <t@mx>
2023-07-15 16:30:01 -0400

    ch4 video 1 around less than half working kept for safety

--- pages/post/new.js
+++ pages/post/new.js
@@ -3,7 +3,21 @@ import { AppLayout } from "../../components/AppLayout";
 
 export default function NewPost(props) {
   console.log("NEW POST PROPS: ", props);
-  return <div>this is the new post page</div>;
+  const handleClick = async () => {
+    const response = await fetch("/api/generatePost", {
+      method: "POST",
+    });
+    const json = await response.json();
+    console.log("Results : ", json);
+  };
+  return (
+    <div>
+      <h1>this is the new post page</h1>
+      <button className="btn" onClick={handleClick}>
+        Generate
+      </button>
+    </div>
+  );
 }
 NewPost.getLayout = function getLayout(page, pageProps) {
   return <AppLayout {...pageProps}>{page}</AppLayout>;
