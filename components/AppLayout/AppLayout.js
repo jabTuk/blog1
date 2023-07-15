674b07e26db5c4e772419c276e554b6b6468ace8
t <t@mx>
2023-07-15 12:59:07 -0400

    ch3 video3 final  work upto here

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -15,20 +15,25 @@ export const AppLayout = ({ children }) => {
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
           list of posts
         </div>
-        <div className="bg-cyan-800">
-          {" "}
+        <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
           {!!user ? (
             <>
-              <div>
+              <div className="min-w-[50px]">
                 <Image
                   src={user.picture}
                   alt={user.name}
                   height={50}
                   width={50}
+                  className="rounded-full"
                 />
-                <div>{user.email}</div>
               </div>
-              <Link href="/api/auth/logout">Logout</Link>
+              <div flex-1>
+                <div className="font-bold">{user.email}</div>
+
+                <Link className="text-sm" href="/api/auth/logout">
+                  Logout
+                </Link>
+              </div>
             </>
           ) : (
             <Link href="/api/auth/login">Login</Link>
